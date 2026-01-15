fe28cb9d7a0476fec2571b96b386319fa3d5e9bc
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-01-15 15:49:36 -0500

    lesson finished

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+    label: {
+        type: String,
+        default: '',
+    },
+    modelValue: {
+        type: [String, Number],
+        default: '',
+    },
+    options: {
+        type: Array,
+        required: true,
+    }
+})
+</script>
+<template>
+    <label v-if="label">{{ label }}</label>
+      <select 
+        v-bind="
+            { 
+                ...$attrs,
+                onChange: ($event) => { $emit('update:modelValue', $event.target.value) }
+             }
+        "
+        :value="modelValue"
+        class="field"
+      >
+        <option
+          v-for="option in options"
+          :value="option"
+          :key="option"
+          :selected="option === modelValue"
+        >{{ option }}</option>
+      </select>
+</template>
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -2,16 +2,11 @@
   <div>
     <h1>Create an event</h1>
     <form>
-
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option
-          v-for="option in categories"
-          :value="option"
-          :key="option"
-          :selected="option === event.category"
-        >{{ option }}</option>
-      </select>
+      <BaseSelect 
+        v-model="event.category"
+        label="Select a category"
+        :options="categories"
+      />
 
       <h3>Name & describe your event</h3>
 
@@ -83,6 +78,7 @@
 </template>
 
 <script>
+
 export default {
   data () {
     return {
